216a35eab0ecdcff4acc38ae7e4d520cd3872f59
Norman Lynch <lynchnf@gmail.com>
2019-09-14 21:20:27 -0400

    Unit tests for Json utils.

--- src/main/java/norman/bunch/of/things/JsonUtils.java
+++ src/main/java/norman/bunch/of/things/JsonUtils.java
@@ -7,18 +7,21 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class JsonUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonUtils.class);
-    private static ObjectMapper objectMapper = new ObjectMapper();
+    private static ObjectMapper mapper = new ObjectMapper();
 
     private JsonUtils() {
     }
 
     public static JsonNode fileToJson(File file) throws LoggingException {
         try {
-            return objectMapper.readTree(file);
+            return mapper.readTree(file);
         } catch (IOException e) {
             throw new LoggingException(LOGGER,
                     "Unable to read file as JSON, file=" + (file == null ? null : file.getAbsolutePath()), e);
@@ -28,39 +31,70 @@ public class JsonUtils {
     public static JsonNode fileToJsonIgnoreErrors(File file) {
         JsonNode jsonNode = null;
         try {
-            jsonNode = objectMapper.readTree(file);
+            jsonNode = mapper.readTree(file);
         } catch (IOException ignored) {
         }
         return jsonNode;
     }
 
-    public static Boolean jsonToBoolean(JsonNode jsonNode) {
+    public static String jsonTreeToString(JsonNode jsonNode) {
+        return jsonNode.toString();
+    }
+
+    public static List<String> jsonValueToListOfStrings(JsonNode jsonNode) throws LoggingException {
+        if (!jsonNode.isArray()) {
+            throw new LoggingException(LOGGER, "Json Node is not an array. jsonNode=" + jsonNode);
+        }
+        Iterator<JsonNode> elements = jsonNode.elements();
+        List<String> strings = new ArrayList<>();
+        while (elements.hasNext()) {
+            strings.add(elements.next().asText());
+        }
+        return strings;
+    }
+
+    public static Boolean jsonValueToBoolean(JsonNode jsonNode) throws LoggingException {
+        if (!jsonNode.isBoolean()) {
+            throw new LoggingException(LOGGER, "Json Node is not a boolean. jsonNode=" + jsonNode);
+        }
         return jsonNode.booleanValue();
     }
 
-    public static Double jsonToDouble(JsonNode jsonNode) {
+    public static Double jsonValueToDouble(JsonNode jsonNode) throws LoggingException {
+        if (!jsonNode.isDouble()) {
+            throw new LoggingException(LOGGER, "Json Node is not a double. jsonNode=" + jsonNode);
+        }
         return jsonNode.doubleValue();
     }
 
-    public static Integer jsonToInteger(JsonNode jsonNode) {
+    public static Integer jsonValueToInteger(JsonNode jsonNode) throws LoggingException {
+        if (!jsonNode.isInt()) {
+            throw new LoggingException(LOGGER, "Json Node is not an integer. jsonNode=" + jsonNode);
+        }
         return jsonNode.intValue();
     }
 
-    public static Map jsonToMap(JsonNode jsonNode) {
-        return objectMapper.convertValue(jsonNode, Map.class);
+    public static Map jsonToMap(JsonNode jsonNode) throws LoggingException {
+        if (!jsonNode.isObject()) {
+            throw new LoggingException(LOGGER, "Json Node is not an object. jsonNode=" + jsonNode);
+        }
+        return mapper.convertValue(jsonNode, Map.class);
     }
 
-    public static String jsonToString(JsonNode jsonNode) {
+    public static String jsonValueToString(JsonNode jsonNode) throws LoggingException {
+        if (!jsonNode.isTextual()) {
+            throw new LoggingException(LOGGER, "Json Node is not text. jsonNode=" + jsonNode);
+        }
         return jsonNode.textValue();
     }
 
     public static JsonNode mapToJson(Map map) {
-        return objectMapper.valueToTree(map);
+        return mapper.valueToTree(map);
     }
 
-    public static JsonNode stringToJson(String jsonString) throws LoggingException {
+    public static JsonNode stringToJsonTree(String jsonString) throws LoggingException {
         try {
-            return objectMapper.readTree(jsonString);
+            return mapper.readTree(jsonString);
         } catch (IOException e) {
             throw new LoggingException(LOGGER, "Unable to read string as JSON, string=" + jsonString, e);
         }
---
+++ src/test/java/norman/bunch/of/things/JsonUtilsTest.java
@@ -0,0 +1,295 @@
+package norman.bunch.of.things;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class JsonUtilsTest {
+    public static final String[] fieldsNames =
+            new String[]{"arrayField", "booleanField", "doubleField", "integerField", "mapField", "nullField",
+                    "stringField" };
+    private JsonNode objectNode;
+
+    @Before
+    public void setUp() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objectNode = mapper.createObjectNode();
+
+        // Array of strings.
+        ArrayNode arrayNode = mapper.createArrayNode();
+        arrayNode.add("one");
+        arrayNode.add("two");
+        objectNode.set("arrayField", arrayNode);
+
+        // Boolean.
+        objectNode.put("booleanField", true);
+
+        // Double.
+        objectNode.put("doubleField", 123.45);
+
+        // Integer.
+        objectNode.put("integerField", 12345);
+
+        // Complex map with string, array, object.
+        ObjectNode mapNode = mapper.createObjectNode();
+        String string = "foo";
+        mapNode.put("string", string);
+        ArrayNode array = mapper.createArrayNode();
+        array.add("bar");
+        array.add("baz");
+        mapNode.set("array", array);
+        ObjectNode object = mapper.createObjectNode();
+        object.put("qux", "thud");
+        mapNode.set("object", object);
+        objectNode.set("mapField", mapNode);
+
+        // Null.
+        objectNode.putNull("nullField");
+
+        // String.
+        objectNode.put("stringField", "string value");
+
+        this.objectNode = objectNode;
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        objectNode = null;
+    }
+
+    @Test
+    public void fileToJson() throws Exception {
+        // Good file contents should convert to a non-null json node without blowing up.
+        URL url = Thread.currentThread().getContextClassLoader().getResource("data/good.json");
+        File file = new File(url.toURI());
+        JsonNode jsonNode = JsonUtils.fileToJson(file);
+        assertNotNull(jsonNode);
+    }
+
+    @Test
+    public void fileToJsonWithException() throws Exception {
+        // Bad file contents should blow up.
+        URL url = Thread.currentThread().getContextClassLoader().getResource("data/bad.json");
+        File file = new File(url.toURI());
+        boolean exceptionThrown = false;
+        try {
+            JsonUtils.fileToJson(file);
+        } catch (LoggingException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void fileToJsonIgnoreErrors() throws Exception {
+        // Bad file contents should convert to a null.
+        URL url = Thread.currentThread().getContextClassLoader().getResource("data/bad.json");
+        File file = new File(url.toURI());
+        JsonNode jsonNode = JsonUtils.fileToJsonIgnoreErrors(file);
+        assertNull(jsonNode);
+    }
+
+    @Test
+    public void jsonTreeToString() throws Exception {
+        // A full json object should convert to a string which can be converted back into json.
+        String string = JsonUtils.jsonTreeToString(objectNode);
+        assertNotNull(string);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(string);
+        assertNotNull(jsonNode);
+
+        // This json string should contain all the field names.
+        for (String fieldName : fieldsNames) {
+            assertTrue(string.contains(fieldName));
+        }
+    }
+
+    @Test
+    public void jsonValueToListOfStrings() throws Exception {
+        // Only the array field should convert to a list of strings. All other should blow up.
+        for (String fieldName : fieldsNames) {
+            JsonNode jsonNode = objectNode.get(fieldName);
+            if (fieldName.equals("arrayField")) {
+                List<String> value = JsonUtils.jsonValueToListOfStrings(jsonNode);
+                assertEquals(2, value.size());
+                assertEquals("one", value.get(0));
+                assertEquals("two", value.get(1));
+            } else {
+                boolean exceptionThrown = false;
+                try {
+                    JsonUtils.jsonValueToListOfStrings(jsonNode);
+                } catch (LoggingException e) {
+                    exceptionThrown = true;
+                }
+                assertTrue(exceptionThrown);
+            }
+        }
+    }
+
+    @Test
+    public void jsonValueToBoolean() throws Exception {
+        // Only the boolean field should convert to a boolean value. All other should blow up.
+        for (String fieldName : fieldsNames) {
+            JsonNode jsonNode = objectNode.get(fieldName);
+            if (fieldName.equals("booleanField")) {
+                Boolean value = JsonUtils.jsonValueToBoolean(jsonNode);
+                assertTrue(value);
+            } else {
+                boolean exceptionThrown = false;
+                try {
+                    JsonUtils.jsonValueToBoolean(jsonNode);
+                } catch (LoggingException e) {
+                    exceptionThrown = true;
+                }
+                assertTrue(exceptionThrown);
+            }
+        }
+    }
+
+    @Test
+    public void jsonValueToDouble() throws Exception {
+        // Only the double field should convert to a double value. All other should blow up.
+        for (String fieldName : fieldsNames) {
+            JsonNode jsonNode = objectNode.get(fieldName);
+            if (fieldName.equals("doubleField")) {
+                Double value = JsonUtils.jsonValueToDouble(jsonNode);
+                assertEquals(123.45, value, 0.005);
+            } else {
+                boolean exceptionThrown = false;
+                try {
+                    JsonUtils.jsonValueToDouble(jsonNode);
+                } catch (LoggingException e) {
+                    exceptionThrown = true;
+                }
+                assertTrue(exceptionThrown);
+            }
+        }
+    }
+
+    @Test
+    public void jsonValueToInteger() throws Exception {
+        // Only the integer field should convert to an integer value. All other should blow up.
+        for (String fieldName : fieldsNames) {
+            JsonNode jsonNode = objectNode.get(fieldName);
+            if (fieldName.equals("integerField")) {
+                Integer value = JsonUtils.jsonValueToInteger(jsonNode);
+                assertEquals(12345, value.intValue());
+            } else {
+                boolean exceptionThrown = false;
+                try {
+                    JsonUtils.jsonValueToInteger(jsonNode);
+                } catch (LoggingException e) {
+                    exceptionThrown = true;
+                }
+                assertTrue(exceptionThrown);
+            }
+        }
+    }
+
+    @Test
+    public void jsonToMap() throws Exception {
+        // Only the map field should convert to a complex map. All other should blow up.
+        for (String fieldName : fieldsNames) {
+            JsonNode jsonNode = objectNode.get(fieldName);
+            if (fieldName.equals("mapField")) {
+                Map value = JsonUtils.jsonToMap(jsonNode);
+                assertEquals(3, value.size());
+
+                Object stringValue = value.get("string");
+                assertTrue(stringValue instanceof String);
+                assertEquals("foo", stringValue);
+
+                Object arrayValue = value.get("array");
+                assertTrue(arrayValue instanceof List);
+                List castToList = (List) arrayValue;
+                assertEquals(2, castToList.size());
+                assertEquals("bar", castToList.get(0));
+                assertEquals("baz", castToList.get(1));
+
+                Object objectValue = value.get("object");
+                assertTrue(objectValue instanceof Map);
+                Map castToMap = (Map) objectValue;
+                assertEquals(1, castToMap.size());
+                assertEquals("thud", castToMap.get("qux"));
+            } else {
+                boolean exceptionThrown = false;
+                try {
+                    JsonUtils.jsonToMap(jsonNode);
+                } catch (LoggingException e) {
+                    exceptionThrown = true;
+                }
+                assertTrue(exceptionThrown);
+            }
+        }
+    }
+
+    @Test
+    public void jsonValueToString() throws Exception {
+        // Only the string field should convert to a string value. All other should blow up.
+        for (String fieldName : fieldsNames) {
+            JsonNode jsonNode = objectNode.get(fieldName);
+            if (fieldName.equals("stringField")) {
+                String value = JsonUtils.jsonValueToString(jsonNode);
+                assertEquals("string value", value);
+            } else {
+                boolean exceptionThrown = false;
+                try {
+                    JsonUtils.jsonValueToString(jsonNode);
+                } catch (LoggingException e) {
+                    exceptionThrown = true;
+                }
+                assertTrue(exceptionThrown);
+            }
+        }
+    }
+
+    @Test
+    public void mapToJson() {
+        // A complex map object should convert to json without blowing up.
+        Map map = new LinkedHashMap();
+        map.put("one", 2);
+        map.put("three", "four");
+        map.put("five", new String[]{"six", "seven" });
+        Map map2 = new HashMap();
+        map2.put("eight", "nine");
+        map.put("ten", map2);
+
+        JsonNode jsonNode = JsonUtils.mapToJson(map);
+        assertNotNull(jsonNode);
+    }
+
+    @Test
+    public void stringToJsonTree() throws Exception {
+        // A good json string should convert to json without blowing up.
+        String goodJsonString =
+                "{\"ant\":99,\"bat\":\"cat\",\"dog\":[\"elephant\",\"flea\"],\"goat\":{\"horse\":\"iguana\"}}";
+        JsonNode jsonNode = JsonUtils.stringToJsonTree(goodJsonString);
+        assertNotNull(jsonNode);
+    }
+
+    @Test
+    public void stringToJsonTreeWithException() throws Exception {
+        // A bad json string should blow up.
+        String badJsonString = "This is a test. This is only a test.";
+        boolean exceptionThrown = false;
+        try {
+            JsonUtils.stringToJsonTree(badJsonString);
+        } catch (LoggingException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+    }
+}
