7124cbe37e9cff7efdcf69195c29a9f1c717587a
Norman Lynch <lynchnf@gmail.com>
2019-10-19 20:19:14 -0500

    Start breaking up project into separate modules.

---
+++ bunch-of-things-core/src/test/java/norman/bunch/of/things/core/AppTest.java
@@ -0,0 +1,21 @@
+package norman.bunch.of.things.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+//import static org.junit.Assert.*;
+
+public class AppTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+}
